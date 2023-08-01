7f7dd57a30f2731e194d78a55d27ff298661eff0
spencer-rafada <spencer.byui@gmail.com>
2023-08-01 13:47:12 -0600

    added about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import InfoSection from './components/InfoSection'
+import About from './components/About'
 
 export default function App() {
   return (
     <div className='mainSection'>
       <div className='mainSection__grid'>
         <InfoSection />
-        <div className='displaySection'>X</div>
+        <div className='displaySection'>
+          <About />
+        </div>
       </div>
     </div>
   )
